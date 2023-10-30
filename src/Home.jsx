436c1ad14a13ae2fdd0c43222e07b9369c794fbc
miromar <miro.mariapori@metropolia.fi>
2023-10-30 19:39:42 +0200

    Lesson 7

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello, ninjas!", e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log("Hello " + name, e);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            {/* Note: To not invoke the function during creation 
+                use anonymous function like below */}
+            <button onClick={(e) => { handleClickAgain("mario", e) }}>Click me again</button>
         </div>
     );
 }
