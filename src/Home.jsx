a6aa1d15410b1f19d7eb8d4b9473708380cf9490
miromar <miro.mariapori@metropolia.fi>
2023-10-30 19:48:55 +0200

    Lesson 8

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log("Hello, ninjas!", e);
-    }
+    const [name, setName] = useState('mario')
+    const [age, setAge] = useState(25);
 
-    const handleClickAgain = (name, e) => {
-        console.log("Hello " + name, e);
+    const handleClick = () => {
+        setName('luigi');
+        setAge(30);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            {/* Note: To not invoke the function during creation 
-                use anonymous function like below */}
-            <button onClick={(e) => { handleClickAgain("mario", e) }}>Click me again</button>
         </div>
     );
 }
 
 export default Home;
+
+/*
+Notes:
+To not invoke the function during creation use anonymous function like this:
+             
+<button onClick={(e) => { handleClickAgain("mario", e) }}>Click me again</button>
+
+e is the event and is passed as an argument to the anonymous function and then passed to handleClickAgain function.
+
+*/
